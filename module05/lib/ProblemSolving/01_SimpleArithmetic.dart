b477e7eafabfdb4f81736caac92a32fcf36adbd5
Md Shamim Ahamed <md.shamim.ahamed.888@gmail.com>
2023-09-02 11:22:55 +0600

    problem solving

--- module05/lib/ProblemSolving/01_SimpleArithmetic.dart
+++ module05/lib/ProblemSolving/01_SimpleArithmetic.dart
@@ -1,3 +1,42 @@
+import 'dart:io';
+
 void main(){
-  print('Hello World');
+  List<String> num = stdin.readLineSync()!.split(' ');
+  int a = int.parse(num[0]);
+  int b = int.parse(num[2]);
+
+  switch (num[1]){
+    case '+':
+      print(add(a, b));
+      break;
+    case '-':
+      print(subtract(a, b));
+      break;
+    case '*':
+      print(multiply(a, b));
+      break;
+    case '/':
+      if(b != 0) {
+        print(divide(a, b).toStringAsFixed(2));
+      }
+      break;
+    default:
+      print('Invalid operator');
+  }
+}
+
+int add(int a, int b){
+  return a + b;
+}
+
+int subtract(int a, int b){
+  return a - b;
+}
+
+int multiply(int a, int b){
+  return a * b;
+}
+
+double divide(int a, int b){
+  return a / b;
 }
