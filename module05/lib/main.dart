a5dd4e0542452afb01e656cf1bf47368eb8c7d8e
shamim36 <md.shamim.ahamed.888@gmail.com>
2023-09-08 23:28:38 +0600

    Module 05-part03

--- module05/lib/main.dart
+++ module05/lib/main.dart
@@ -8,62 +8,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: true,
-      home: HomeScreen(),
+      home: ProfileActivity(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class ProfileActivity extends StatelessWidget {
+  const ProfileActivity({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        toolbarHeight: 100,
-        elevation: 70,
-        title: const Text(
-          'Home',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        toolbarHeight: 80,
+        backgroundColor: Colors.blue,
+        title: const Center(
+          child: Text(
+            'Profile',
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
         ),
-        leading: const Icon(Icons.add_business_rounded, color: Colors.white),
-        actions: const [
-          Icon(Icons.search, color: Colors.white),
-        ],
       ),
       body: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'This is mod 5 Assignment',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold),
-            ),
-            Row(
+            Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                RichText(
-                  text: const TextSpan(
-                    style: TextStyle(fontSize: 23, color: Colors.red),
-                    text: 'My ',
-                    children: [
-                      TextSpan(
-                          text: 'phone ',
-                          style: TextStyle(fontSize: 13, color: Colors.blue)),
-                      TextSpan(
-                          text: 'name ',
-                          style: TextStyle(fontSize: 16, color: Colors.pink)),
-                      TextSpan(
-                          text: 'Your phone name ',
-                          style: TextStyle(color: Colors.orange)),
-                    ],
-                  ),
+                Text(
+                  'Jhon Doe',
+                  style: TextStyle(fontSize: 40, color: Colors.green),
+                ),
+                Text(
+                  'Flutter Batch 4',
+                  style: TextStyle(fontSize: 40, color: Colors.blue),
                 ),
               ],
             ),
